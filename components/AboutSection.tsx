15166ab70c3509c2e51584e410cf9a0fc9e963fd
BraydenTan <the7corptech@gmail.com>
2023-02-13 14:23:53 +0800

    name changed

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -30,7 +30,7 @@ const AboutSection = () => {
               Get to know me!
             </h1>
             <p>
-              Hi, my name is Brendan Tan and I am a{" "}
+              Hi, my name is Breydan Tan and I am a{" "}
               <span className="font-bold">{"highly ambitious"}</span>,
               <span className="font-bold">{" self-motivated"}</span>, and
               <span className="font-bold">{" driven"}</span> support engineer
